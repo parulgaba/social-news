6cf5de2e420a305f7d52c2ded8cd14c21b66b8c1
Parul Gaba <coolestgaba@gmail.com>
2018-11-01 22:48:17 +0530

    Updates

---
+++ precache-manifest.af7df107ad253288b8bc378f8cf312fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b53a387403e7b3a72224",
+    "url": "/social-news/static/css/main.334baf3b.chunk.css"
+  },
+  {
+    "revision": "b53a387403e7b3a72224",
+    "url": "/social-news/static/js/main.b53a3874.chunk.js"
+  },
+  {
+    "revision": "1f28a60286c1a31a4eed",
+    "url": "/social-news/static/css/1.24af5c36.chunk.css"
+  },
+  {
+    "revision": "1f28a60286c1a31a4eed",
+    "url": "/social-news/static/js/1.1f28a602.chunk.js"
+  },
+  {
+    "revision": "240c787f078adf38ab86",
+    "url": "/social-news/static/js/runtime~main.240c787f.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/social-news/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/social-news/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/social-news/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/social-news/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/social-news/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "2e8cf4262617bd5a867e395824171ffb",
+    "url": "/social-news/index.html"
+  }
+];
