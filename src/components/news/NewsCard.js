b53729308f2856472afa825e3bccd058fa10aee9
Parul Gaba <coolestgaba@gmail.com>
2018-11-02 12:24:23 +0530

    Left align text

--- src/components/news/NewsCard.js
+++ src/components/news/NewsCard.js
@@ -25,7 +25,7 @@ class NewsCard extends React.Component {
     const { open } = this.state;
     const publishedDate = new Date(article.published_date);
     return (
-      <div class="col s12 m8 offset-m2 l6 offset-l3">
+      <div class="col s12 m8 offset-m2 l6 offset-l3 left-align">
         <div class="card-panel grey lighten-5 z-depth-1">
           <div class="row valign-wrapper">
             <div class="col s2">
